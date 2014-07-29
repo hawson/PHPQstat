1b4f20be6e33003c086b99d04707e0f34ee2ca8a
Jesse Becker <hawson@gmail.com>
2014-07-29 10:36:49 -0400

    Update accounting.sh to generate quota charts

--- accounting.sh
+++ accounting.sh
@@ -124,3 +124,33 @@ rrdtool graph $WEB_ROOT/img/qw_2week.png $STD_OPTS -s -2week  -t "Pending Jobs (
 rrdtool graph $WEB_ROOT/img/qw_month.png $STD_OPTS -s -1month -t "Pending Jobs (Monthly)" -v "Queued jobs" COMMENT:'                    ' COMMENT:"Min"  COMMENT:" Max"  COMMENT:"  Avg" COMMENT:" Last\\l"   $pending_extrema $pending_datagrups_LINE $pending_status  COMMENT:"Last update\: $DATE" 
 rrdtool graph $WEB_ROOT/img/qw_year.png  $STD_OPTS -s -1year  -t "Pending Jobs (Yearly)"  -v "Queued jobs" COMMENT:'                    ' COMMENT:"Min"  COMMENT:" Max"  COMMENT:"  Avg" COMMENT:" Last\\l"   $pending_extrema $pending_datagrups_LINE $pending_status  COMMENT:"Last update\: $DATE" 
 
+
+
+# qacct_quota_gpipe_limit.rrd
+
+unset quotagroups
+unset QUOTAS
+i=0
+for f in $RRD_ROOT/*_quota*.rrd; do 
+    FILE=`basename $f`
+    QUOTA=${FILE/qacct_quota_/};
+    QUOTA=${QUOTA/_limit.rrd/};
+    QUOTAS="$QUOTA $QUOTAS"
+    DEF="$quotagroups DEF:${QUOTA}-used=$RRD_ROOT/$FILE:used:AVERAGE DEF:${QUOTA}-avail=$RRD_ROOT/$FILE:avail:AVERAGE "
+    VDEF="VDEF:${QUOTA}-min=${QUOTA}-used,MINIMUM    VDEF:${QUOTA}-max=${QUOTA}-used,MAXIMUM   VDEF:${QUOTA}-avg=${QUOTA}-used,AVERAGE    VDEF:${QUOTA}-last=${QUOTA}-used,LAST"
+    LINE="LINE1:${QUOTA}-used#${COLOR[${i}]}:$QUOTA   LINE1:${QUOTA}-avail#${COLOR[${i}]}:"":dashes=3"
+    pad=$((20-${#QUOTA}))
+    STATS="GPRINT:${QUOTA}-min:%${pad}.0lf GPRINT:${QUOTA}-max:%5.0lf GPRINT:${QUOTA}-avg:%5.0lf GPRINT:${QUOTA}-last:%5.0lf\\l" 
+    quotagroups="$DEF $VDEF $LINE $STATS  "
+    i=$((i+1))
+done
+
+RULE=""
+
+rrdtool graph $WEB_ROOT/img/quota_hour.png  $STD_OPTS -s -1hour  -t "Jobs by Quota (hourly)"  -v "Jobs" COMMENT:'                    ' COMMENT:"Min"  COMMENT:" Max"  COMMENT:"  Avg" COMMENT:" Last\\l"   $quotagroups  $LINES  $RULE  COMMENT:"Last update\: $DATE" 
+rrdtool graph $WEB_ROOT/img/quota_day.png   $STD_OPTS -s -1day   -t "Jobs by Quota (daily)"   -v "Jobs" COMMENT:'                    ' COMMENT:"Min"  COMMENT:" Max"  COMMENT:"  Avg" COMMENT:" Last\\l"   $quotagroups  $LINES  $RULE  COMMENT:"Last update\: $DATE" 
+rrdtool graph $WEB_ROOT/img/quota_week.png  $STD_OPTS -s -1week  -t "Jobs by Quota (Weekly)"  -v "Jobs" COMMENT:'                    ' COMMENT:"Min"  COMMENT:" Max"  COMMENT:"  Avg" COMMENT:" Last\\l"   $quotagroups  $LINES  $RULE  COMMENT:"Last update\: $DATE" 
+rrdtool graph $WEB_ROOT/img/quota_2week.png $STD_OPTS -s -2week  -t "Jobs by Quota (Weekly)"  -v "Jobs" COMMENT:'                    ' COMMENT:"Min"  COMMENT:" Max"  COMMENT:"  Avg" COMMENT:" Last\\l"   $quotagroups  $LINES  $RULE  COMMENT:"Last update\: $DATE" 
+rrdtool graph $WEB_ROOT/img/quota_month.png $STD_OPTS -s -1month -t "Jobs by Quota (Monthly)" -v "Jobs" COMMENT:'                    ' COMMENT:"Min"  COMMENT:" Max"  COMMENT:"  Avg" COMMENT:" Last\\l"   $quotagroups  $LINES  $RULE  COMMENT:"Last update\: $DATE" 
+rrdtool graph $WEB_ROOT/img/quota_year.png  $STD_OPTS -s -1year  -t "Jobs by Quota (Yearly)"  -v "Jobs" COMMENT:'                    ' COMMENT:"Min"  COMMENT:" Max"  COMMENT:"  Avg" COMMENT:" Last\\l"   $quotagroups  $LINES  $RULE  COMMENT:"Last update\: $DATE" 
+
