4065f5f9c09b34309c780e236ff6a04d15fa5842
Jesse Becker <hawson@gmail.com>
2016-02-10 11:20:18 -0500

    Fix queue display on UGE.  Fix job usage display as well.

--- qstat_job.php
+++ qstat_job.php
@@ -51,7 +51,7 @@ $job_group=$qstat->djob_info->element[0]->JB_group;
 $job_project=$qstat->djob_info->element[0]->JB_project;
 $job_ust=$qstat->djob_info->element[0]->JB_submission_time;
 $job_st=date('r', date($job_ust * 1 ) );
-$job_qn=$qstat->djob_info->element[0]->JB_hard_queue_list->destin_ident_list->QR_name;
+$job_qn=$qstat->djob_info->element[0]->JB_ja_tasks->ulong_sublist->JAT_grandted_destin_identifier_list->JG_qname;
 $job_pe=$qstat->djob_info->element[0]->JB_pe;
 $job_slots=$qstat->djob_info->element[0]->JB_pe_range->ranges->RN_min;
 
@@ -84,32 +84,37 @@ echo "	<table align=center width=95% border=\"1\" cellpadding=\"0\" cellspacing=
 
 
 $i=0;
+$usage_stats = array();
 foreach ($qstat->xpath('//scaled') as $usage) {
-$usage_stats[$i++]=$usage->UA_value;
+    $key=strval($usage->UA_name);
+    $val=strval($usage->UA_value);
+    $usage_stats[$key]=$val;
 }
-if ($usage_stats[0] > 0){$cputime = time_duration($usage_stats[0], 'dhms');}else{$cputime = 0;}
+
+$cputime = ($usage_stats['cpu'] > 0) ?  $cputime = time_duration($usage_stats['cpu'], 'dhms') : 0;
+
 echo "	<table align=center width=95% border=\"1\" cellpadding=\"0\" cellspacing=\"0\">
         <tbody>
 		<tr CLASS=\"header\">
-		<td>CPUTime (s)</td>
-                <td>Mem (GB)</td>
-                <td>io</td>
-                <td>iow</td>
-                <td>VMem (M)</td>
-                <td>MaxVMem (M)</td>
-                </tr>
-                <tr>
-                <td>$cputime</td>
-                <td>".number_format($usage_stats[1], 2, '.', '')."</td>
-                <td>".number_format($usage_stats[2], 2, '.', '')."</td>
-                <td>".number_format($usage_stats[3], 2, '.', '')."</td>
-                <td>".number_format($usage_stats[4]/1024/1024, 2, '.', '')."</td>
-                <td>".number_format($usage_stats[5]/1024/1024, 2, '.', '')."</td>
-              </tr>	  
-           </tbody>
+            <td>CPUTime (s)</td>
+            <td>Mem (GB)</td>
+            <td>io</td>
+            <td>iow</td>
+            <td>VMem (M)</td>
+            <td>MaxVMem (M)</td>
+        </tr>
+        <tr>
+            <td>$cputime</td>
+            <td>".number_format($usage_stats['mem'], 2, '.', '')."</td>
+            <td>".number_format($usage_stats['io'], 2, '.', '')."</td>
+            <td>".number_format($usage_stats['iow'], 2, '.', '')."</td>
+            <td>".number_format($usage_stats['vmem']/1024/1024, 2, '.', '')."</td>
+            <td>".number_format($usage_stats['maxvmem']/1024/1024, 2, '.', '')."</td>
+       </tr>	  
+       </tbody>
 	</table><br>";
 
-unlink("/tmp/$token.xml");
+#unlink($tokenfile);
 ?>
 
 
