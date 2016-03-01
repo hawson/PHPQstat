dd1e678283725febcba39fc1e44c7102fa63d555
Jesse Becker <hawson@gmail.com>
2016-03-01 15:58:24 -0500

    Update authors

--- about.php
+++ about.php
@@ -28,7 +28,7 @@ echo "<tr><td><h1>PHPQstat</h1></td></tr>
 <table align=center width=50% border="0" cellpadding="0" cellspacing="0">
 <tr><td align=center>
 <b>PHPQstat</b> is a web interface that allows to connect to the usefull commands of the Grid Engine (GE) batch queue system. With this interface, you can monitor your job status and your queues health at real time.<br><br>
-<b>AUTHOR</b> Written by Jordi Blasco Pallarès.<br>
+<b>AUTHOR</b> Written by Jordi Blasco Pallarès, with later improvements by Lydia Sevelt and Jesse Becker<br>
 <b>REPORTING BUGS</b> Report bugs to <a href="https://github.com/hawson/PHPQstat">GitHub</a>
 <b>LICENSE</b> This is free software: you are free to change and redistribute it. GNU General Public License version 3.0 (<a href=http://gnu.org/licenses/gpl.html target=gpl>GPLv3</a>).<br>
 <b>Version : 0.3.0 (February 2012)</b><br><br>
