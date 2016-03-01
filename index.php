efc773ed7d330fe8c985eecb91c43c643c2ececf
Jesse Becker <hawson@gmail.com>
2016-03-01 15:49:00 -0500

    Just redirect to overview page.

--- index.php
+++ index.php
@@ -7,36 +7,9 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="KEYWORDS" content="gridengine sge sun hpc supercomputing batch queue linux xml qstat qhost jordi blasco solnu">
   <link rel="stylesheet" href="phpqstat.css" type="text/css" /> 
- 
+  <meta http-equiv="refresh" content="0;URL=qstat.php">
 </head>
 <body>
-<br><br><br><br><br><br><br>
-<table align=center width=30% border="1" cellpadding="0" cellspacing="0">
-  <tbody>
-    <tr>
-      <td align=center><h1>PHPQstat</h1></td>
-    </tr>
-    <tr>
-      <td align=center>
-      <FORM ACTION=qstat.php METHOD=GET> 
-      User: <input size=10 name=owner type=text value=all><input type=submit value='Enter'> 
-      </FORM>  
-      </td>
-    </tr>
-
-
-    <tr> 
-      <td align=center CLASS="bottom"> 
-        version : 0.3.0 (31 July 2014)<br>
-        <a href="https://github.com/hawson/PHPQstat">https://github.com/hawson/PHPQstat</a><br>
-      </td> 
-    </tr> 
-
-
-  </tbody>
-</table>
-
-
 </body>
 </html>
 
